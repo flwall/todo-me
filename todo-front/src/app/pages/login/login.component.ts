a0d6f372c4dd75dbfa7650319649892660cf30a2
MrTob <mr.tobtv@gmail.com>
2020-04-18 17:52:06 +0200

    changed email to user

--- todo-front/src/app/pages/login/login.component.ts
+++ todo-front/src/app/pages/login/login.component.ts
@@ -16,9 +16,9 @@ export class LoginComponent implements OnInit {
   loginUser(event) {
     event.preventDefault();
     const target = event.target;
-    const email = target.querySelector('#email').value;
+    const user = target.querySelector('#user').value;
     const password = target.querySelector('#password').value;
-    this.Auth.loginUser(email, password);
-    console.log(email, password);
+    this.Auth.loginUser(user, password);
+    console.log('Input from user:' + user, password);
   }
 }
