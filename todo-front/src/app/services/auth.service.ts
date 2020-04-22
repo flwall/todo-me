fb6d0b6136ae4970265b20a312303aa9033e5988
MrTob <mr.tobtv@gmail.com>
2020-04-22 17:28:40 +0200

    cookie issue

--- todo-front/src/app/services/auth.service.ts
+++ todo-front/src/app/services/auth.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { AuthUser } from '../models/authUser';
+import {AuthUser} from '../models/authUser';
+import {Observable} from 'rxjs';
+import {Todo} from '../models/todo';
+import 'rxjs/add/operator/map';
+import {delay} from 'rxjs/operators';
 
 interface Data {
   sucess: boolean;
@@ -16,12 +20,25 @@ export class AuthService {
   loggedInStatus = false;
 
   private API_URL = 'https://localhost:9011/api/';
-  constructor(private  http: HttpClient) { }
 
-  loginUser(user, passwrd) {
+  constructor(private  http: HttpClient) {
+  }
+
+  private cookie = 'DEFAULT';
+
+  async loginUser(user, passwrd) {
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
     const u: AuthUser = {username: user, password: passwrd};
-    return this.http.post(this.API_URL + 'auth/login/', u, {headers, responseType: 'text'});
+    const sth = this.http.post(this.API_URL + 'auth/login/', u, {headers, responseType: 'text', observe: 'response'});
+
+    sth.map((response) => {
+      this.cookie = response.headers.get('Set-Cookie');
+
+    });
+    await delay(3000);
+
+    return sth;
+
   }
 
   setLoggedIn(b: boolean) {
@@ -30,5 +47,18 @@ export class AuthService {
 
   get isLoggedIn() {
     return this.loggedInStatus;
+  }
+
+  getTodos(): Observable<Todo[]> {
+    console.log(this.cookie);
+    const headers = new HttpHeaders();
+    headers.set('Set-Cookie', this.cookie);
+    return this.http.get<Todo[]>(this.API_URL + 'todos', {headers});
+    /*
+      .toPromise()
+      .then(response => response.map(i => new Todo(i.todoID, i.title, i.description, i.createtAt, i.done )))
+      .catch(error => {
+        console.log(error);
+      });*/
   }
 }
